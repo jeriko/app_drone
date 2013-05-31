36b537836df0973dbe2006689eeff2f0d1d27dd1
David Kerber <dave@agapered.com>
2013-05-31 20:39:57 +0000

    adding the ability to load drones from a path outside the gem without modifying the default behavior

--- lib/app_drone.rb
+++ lib/app_drone.rb
@@ -6,11 +6,13 @@ require 'active_support/inflector'
 lib_files = %w(version dependency_chain template drone object_extensions)
 lib_files.each { |f| require "app_drone/#{f}" }
 
-# require all drones in app_drone/drones, exclude 'zzz' folder
-drone_paths = Pathname.new(File.dirname(__FILE__) + '/app_drone/drones')
-drones = drone_paths.children.select(&:directory?).map { |path| path.split.last.to_s }
-drones -= ['zzz']
-drones.each { |d| require "app_drone/drones/#{d}/#{d}" }
-
 module AppDrone
+  def self.require_drones_at_path(drone_paths)
+    drone_paths = Pathname.new(drone_paths)
+    drones = drone_paths.children.select(&:directory?).map { |path| path.split.last.to_s }
+    drones -= ['zzz']
+    drones.each { |d| require  "#{drone_paths.to_path}/#{d}/#{d}" }
+  end
 end
+
+AppDrone.require_drones_at_path "#{File.dirname(__FILE__)}/app_drone/drones"
